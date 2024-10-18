33b28bdb030615d0e568b584546072cc98b965fa
pae_adwaa <adu27747g@gmail.com>
2024-10-18 20:08:37 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WU Freshy Awards</title>
+    <link rel="stylesheet" href="css/home.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <div class="left-section">
+            <div class="header">
+                <div class="hover">
+                    <a>WU</a>
+                    <a>Freshy</a>
+                    <a>Awards</a>
+                </div>
+                <p>ระบบกำลังเปิดใช้งาน</p>
+            </div>
+            <div class="content">
+                <p>
+                    เว็บไซต์นี้ทำมาสำหรับการจองที่นั่งสำหรับงาน WU Freshy Awards <br>โดยจัดสถานที่มหาวิทยาลัยวลัยลักษณ์ ณ อาคารไทยบุรี
+                </p>
+                <a href="welcome">
+                    <button>
+                        ลงชื่อเข้าใช้
+                    </button>
+                </a>
+                <div>
+                    <a>ใช้งานไม่เป็นหรอ</a>
+                    <a href="">กดตรงนี้ได้น่ะ</a>
+                </div>
+            </div>
+        </div>
+    </div>
+        <div class="right-section">
+            <div class="box">
+                <div align="center">
+                    <div class="text-Vdo">คลิปวิดีโอแนะนำการใช้งานเว็บไซต์</div>
+                </div>
+                <div>
+                    <iframe width="274.285714286" height="154.285714286" src="Free Green Screen - Realistic Light Bulb.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section>
+        <div class='air air1'></div>
+        <div class='air air2'></div>
+        <div class='air air3'></div>
+        <div class='air air4'></div>
+    </section>
+    <div class="bg-animation">
+        <div id='stars'></div>
+        <div id='stars2'></div>
+        <div id='stars3'></div>
+        <div id='stars4'></div>
+    </div><!-- / STAR ANIMATION --></div>
+</body>
+</html>
